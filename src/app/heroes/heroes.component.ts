0fed48edd7d357f7717d95dd8b8a23439241a036
dennismacc <100378792+dennismacc@users.noreply.github.com>
2023-03-09 05:46:16 -0800

    working on adding click event to heroes, need to fix errors 🔧

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,9 +8,9 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+    selectedHero?: Hero;
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
   heroes = HEROES;
-} 
+};
