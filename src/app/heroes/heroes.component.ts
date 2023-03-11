0fb097438eaa8cfd541c7042948dade83f5d35cc
dennismacc <100378792+dennismacc@users.noreply.github.com>
2023-03-10 16:38:41 -0800

    Styling and final updates to page. Errors resolved (need to look into why error was happening)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,9 +8,9 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
+  heroes = HEROES;
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  heroes = HEROES;
 }
