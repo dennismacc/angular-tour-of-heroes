65f9e0ca833f93df7cd0f5078c99a49b3d87901a
dennismacc <100378792+dennismacc@users.noreply.github.com>
2023-03-10 16:28:19 -0800

    still working on event click handlers and displaying hero details when clicked

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,12 +5,12 @@ import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-    selectedHero?: Hero;
-    onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-    }
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   heroes = HEROES;
-};
+}
